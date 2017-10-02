33604f5816535b138810d5f55e61c4b1d505cd22
Dmitriy Borodin <dmitriy@borodin.site>
2017-10-02 14:50:06 +0300

    [PATCH]

--- src/launchers/copy.ts
+++ src/launchers/copy.ts
@@ -14,6 +14,7 @@ import makeServerConfig from "./makeServerConfig";
 
 async function copy() {
   if (staticServer) {
+    await makeDir(servercOutput);
     await copyFile(path.resolve(__dirname, "./staticServerOrigin.js"), path.resolve(servercOutput, serverEntry));
   }
   if (isIsomorphic || staticServer) {
