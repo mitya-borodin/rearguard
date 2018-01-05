8802068dee6f31754c703f0ded651f9d6586c3b0
Dmitriy Borodin <dmitriy@borodin.site>
2018-01-05 15:07:46 +0300

    [DONE] 1. Add hard-source-webpack-plugin.

--- src/config/webpack.config.ts
+++ src/config/webpack.config.ts
@@ -1,10 +1,11 @@
+import * as HardSourceWebpackPlugin from "hard-source-webpack-plugin";
 import * as path from "path";
 import entry from "./entry";
 import generalWebpackConfig from "./general.webpack.config";
 import { extractCSS } from "./plugins/css";
 import { analyze, clean, definePlugin, DllPlugin, DllReferencePlugin, extractVendors, HMR, htmlWebpackPlugin, scopeHoisting, uglify, workboxPlugin } from "./plugins/js";
 import compiler from "./rules/compiler";
-import { context, dll_entry_name, dll_lib_file_name, dll_lib_name, dll_lib_output_path, isDevelopment, output } from "./target.config";
+import { context, dll_entry_name, dll_lib_file_name, dll_lib_name, dll_lib_output_path, isDevelopment, output, root } from "./target.config";
 
 export const dev = generalWebpackConfig(
   entry(),
@@ -21,6 +22,19 @@ export const dev = generalWebpackConfig(
     ...workboxPlugin(),
     ...htmlWebpackPlugin(),
     ...analyze(),
+    new HardSourceWebpackPlugin({
+      cacheDirectory: path.resolve(root, "node_modules/.cache/hard-source/[confighash]"),
+      configHash: (webpackConfig: any) => {
+        return require("node-object-hash")({ sort: false }).hash(webpackConfig);
+      },
+      environmentHash: {
+        directories: [],
+        files: ["package-lock.json"],
+        root: process.cwd(),
+      },
+      // Sets webpack"s recordsPath if not already set.
+      recordsPath: path.resolve(root, "node_modules/.cache/hard-source/[confighash]/records.json"),
+    }),
   ],
   {},
 );
@@ -42,6 +56,19 @@ export const dll = generalWebpackConfig(
     ...extractCSS(true),
     ...uglify(),
     ...DllPlugin(),
+    new HardSourceWebpackPlugin({
+      cacheDirectory: path.resolve(root, "node_modules/.cache/dll-hard-source/[confighash]"),
+      configHash: (webpackConfig: any) => {
+        return require("node-object-hash")({ sort: false }).hash(webpackConfig);
+      },
+      environmentHash: {
+        directories: [],
+        files: ["package-lock.json"],
+        root: process.cwd(),
+      },
+      // Sets webpack"s recordsPath if not already set.
+      recordsPath: path.resolve(root, "node_modules/.cache/dll-hard-source/[confighash]/records.json"),
+    }),
   ],
   {},
 );
--- src/launchers/declaration.d.ts
+++ src/launchers/declaration.d.ts
@@ -1,2 +1,4 @@
 declare module "workbox-webpack-plugin";
+declare module "hard-source-webpack-plugin";
+declare module "node-object-hash";
 declare module "copy";
