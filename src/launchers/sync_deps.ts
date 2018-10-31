dc4e45f1194d8a76257944df9c1acd14a89c2240
Borodin Dmitriy <dmitriy@borodin.site>
2018-10-31 18:42:21 +0300

    1.  Изменил порядок, следования процедур.

--- src/launchers/sync_deps.ts
+++ src/launchers/sync_deps.ts
@@ -4,10 +4,10 @@ import { sync_npm_deps } from "../config/components/sync.npm.deps";
 import { update_pkg } from "../config/components/update.pkg";
 
 async function run_sync_npm_deps() {
-  await update_pkg();
   pre_publish_shell();
   await ordering_npm_deps();
   await sync_npm_deps();
+  await update_pkg();
 }
 
 run_sync_npm_deps();
