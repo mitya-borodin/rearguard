660dea9c4d4ccf10f2925f4223a0d717a0e65185
Borodin Dmitriy <dmitriy@borodin.site>
2018-11-01 10:44:45 +0300

    1. Вернул горячую перезагрузку по возможности.

--- src/config/components/entry.ts
+++ src/config/components/entry.ts
@@ -1,22 +1,11 @@
 import { Entry, EntryFunc } from "webpack";
-import {
-  entry,
-  isBuild,
-  isDevelopment,
-  isLib,
-  resolveNodeModules,
-  socket,
-} from "./target.config";
+import { entry, isBuild, isDevelopment, isLib, resolveNodeModules, socket } from "./target.config";
 
-export default (
-  entries: string[] = [],
-): string | string[] | Entry | EntryFunc => {
+export default (entries: string[] = []): string | string[] | Entry | EntryFunc => {
   if (isDevelopment && !isBuild && !isLib) {
     return [
-      `${resolveNodeModules("webpack-dev-server")}/client?https://${
-        socket.host
-      }:${socket.port}`,
-      `${resolveNodeModules("webpack")}/hot/dev-server`,
+      `${resolveNodeModules("webpack-dev-server")}/client?https://${socket.host}:${socket.port}`,
+      `${resolveNodeModules("webpack")}/hot/only-dev-server`,
       ...entries,
       entry,
     ];
