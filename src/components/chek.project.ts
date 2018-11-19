5423f36574a52c92322feee325aed141dbe39dff
Дмитрий Бородин <dmitriy@borodin.site>
2018-11-19 12:16:13 +0300

    1. Удалил файл с текстовым шаблоном проверночного скрипта. 2. Создал файл скрипта в отдельном файле, для последующего копирования.

--- src/components/chek.project.ts
+++ src/components/chek.project.ts
@@ -1,26 +1,14 @@
 import chalk from "chalk";
 import * as fs from "fs";
 import * as path from "path";
-import {
-  context,
-  dll_entry,
-  entry as main_entry,
-  isDll,
-  isLib,
-  isWDS,
-  lib_entry,
-} from "./target.config";
+import { context, dll_entry, entry as main_entry, isDll, isLib, isWDS, lib_entry } from "./target.config";
 
 export function check_project() {
   if (isDll) {
     const entry = path.resolve(context, dll_entry);
 
     if (!fs.existsSync(entry)) {
-      console.log(
-        chalk.bold.red(
-          `[ CHECK_PROJECT ][ ERROR ][ dll_entry: ${entry} NOT_FOUND ]`,
-        ),
-      );
+      console.log(chalk.bold.red(`[ CHECK_PROJECT ][ ERROR ][ dll_entry: ${entry} NOT_FOUND ]`));
       console.log("");
 
       process.exit(1);
@@ -31,11 +19,7 @@ export function check_project() {
     const entry = path.resolve(context, lib_entry);
 
     if (!fs.existsSync(entry)) {
-      console.log(
-        chalk.bold.red(
-          `[ CHECK_PROJECT ][ ERROR ][ lib_entry: ${entry} NOT_FOUND ]`,
-        ),
-      );
+      console.log(chalk.bold.red(`[ CHECK_PROJECT ][ ERROR ][ lib_entry: ${entry} NOT_FOUND ]`));
       console.log("");
 
       process.exit(1);
@@ -46,11 +30,7 @@ export function check_project() {
     const entry = path.resolve(context, main_entry);
 
     if (!fs.existsSync(entry)) {
-      console.log(
-        chalk.bold.red(
-          `[ CHECK_PROJECT ][ ERROR ][ entry: ${entry} NOT_FOUND ]`,
-        ),
-      );
+      console.log(chalk.bold.red(`[ CHECK_PROJECT ][ ERROR ][ entry: ${entry} NOT_FOUND ]`));
       console.log("");
 
       process.exit(1);
